410684f921f7bc574ebf04935a6f840be8cfb5d4
topolinocerino2007-ctrl <topolinocerino2007@gmail.com>
2026-01-18 12:18:10 +0100

    Add files via upload

---
+++ src/addons/mod/tabletquiz/accessrules/offlineattempts/services/handlers/offlineattempts.ts
@@ -0,0 +1,84 @@
+// (C) Copyright 2015 Moodle Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Injectable, Type } from '@angular/core';
+
+import { AddonModTabletQuizAccessRuleHandler } from '@addons/mod/tabletquiz/services/access-rules-delegate';
+import { AddonModTabletQuizAttemptWSData, AddonModTabletQuizQuizWSData } from '@addons/mod/tabletquiz/services/quiz';
+import { AddonModTabletQuizSync } from '@addons/mod/tabletquiz/services/quiz-sync';
+import { makeSingleton } from '@singletons';
+
+/**
+ * Handler to support offline attempts access rule.
+ */
+@Injectable({ providedIn: 'root' })
+export class AddonModTabletQuizAccessOfflineAttemptsHandlerService implements AddonModTabletQuizAccessRuleHandler {
+
+    name = 'AddonModTabletQuizAccessOfflineAttempts';
+    ruleName = 'quizaccess_offlineattempts';
+
+    /**
+     * @inheritdoc
+     */
+    getFixedPreflightData(
+        quiz: AddonModTabletQuizQuizWSData,
+        preflightData: Record<string, string>,
+    ): void | Promise<void> {
+        preflightData.confirmdatasaved = '1';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    async getPreflightComponent(): Promise<Type<unknown>> {
+        const { AddonModTabletQuizAccessOfflineAttemptsComponent } = await import('../../component/offlineattempts');
+
+        return AddonModTabletQuizAccessOfflineAttemptsComponent;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    async isEnabled(): Promise<boolean> {
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    async isPreflightCheckRequired(
+        quiz: AddonModTabletQuizQuizWSData,
+        attempt?: AddonModTabletQuizAttemptWSData,
+        prefetch?: boolean,
+    ): Promise<boolean> {
+        if (prefetch) {
+            // Don't show the warning if the user is prefetching.
+            return false;
+        }
+
+        if (!attempt) {
+            // User is starting a new attempt, show the warning.
+            return true;
+        }
+
+        const syncTime = await AddonModTabletQuizSync.getSyncTime(quiz.id);
+
+        // Show warning if last sync was a while ago.
+        return Date.now() - AddonModTabletQuizSync.syncInterval > syncTime;
+    }
+
+}
+
+export const AddonModTabletQuizAccessOfflineAttemptsHandler = makeSingleton(AddonModTabletQuizAccessOfflineAttemptsHandlerService);
+
---
+++ src/addons/mod/tabletquiz/accessrules/timelimit/services/handlers/timelimit.ts
@@ -0,0 +1,68 @@
+// (C) Copyright 2015 Moodle Pty Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Injectable, Type } from '@angular/core';
+
+import { AddonModTabletQuizAccessRuleHandler } from '@addons/mod/tabletquiz/services/access-rules-delegate';
+import { AddonModTabletQuizAttemptWSData, AddonModTabletQuizQuizWSData } from '@addons/mod/tabletquiz/services/quiz';
+import { makeSingleton } from '@singletons';
+
+/**
+ * Handler to support time limit access rule.
+ */
+@Injectable({ providedIn: 'root' })
+export class AddonModTabletQuizAccessTimeLimitHandlerService implements AddonModTabletQuizAccessRuleHandler {
+
+    name = 'AddonModTabletQuizAccessTimeLimit';
+    ruleName = 'quizaccess_timelimit';
+
+    /**
+     * @inheritdoc
+     */
+    async getPreflightComponent(): Promise<Type<unknown>> {
+        const { AddonModTabletQuizAccessTimeLimitComponent } = await import('../../component/timelimit');
+
+        return AddonModTabletQuizAccessTimeLimitComponent;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    async isEnabled(): Promise<boolean> {
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    isPreflightCheckRequired(
+        quiz: AddonModTabletQuizQuizWSData,
+        attempt?: AddonModTabletQuizAttemptWSData,
+    ): boolean | Promise<boolean> {
+        // Warning only required if the attempt is not already started.
+        return !attempt;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    shouldShowTimeLeft(attempt: AddonModTabletQuizAttemptWSData, endTime: number, timeNow: number): boolean {
+        // If this is a teacher preview after the time limit expires, don't show the time left.
+        return !(attempt.preview && timeNow > endTime);
+    }
+
+}
+
+export const AddonModTabletQuizAccessTimeLimitHandler = makeSingleton(AddonModTabletQuizAccessTimeLimitHandlerService);
+
